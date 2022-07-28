93faea17ab90c5d7446158be59fbed3e58aa4196
maksim nasonov <maks.nasonov.03@mail.ru>
2022-07-28 14:48:57 +0700

    add dsbot.js

--- index.js
+++ index.js
@@ -1,99 +1,13 @@
 require('dotenv').config()
-const { Client, GatewayIntentBits, InteractionType} = require('discord.js');
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
 const TelegramApi = require('node-telegram-bot-api')
 const sequelize = require('./db')
 const UserModel = require('./models')
+const {checkVoiceChannels, client} = require('./dsBot')
 
 const tgToken = process.env.tgToken
-const dsToken = process.env.dsToken
-const clientId = process.clientId
 
 const bot = new TelegramApi(tgToken, {polling: true})
 
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates]});
-
-const commands = [
-    new SlashCommandBuilder().setName('countusers').setDescription('Number of users in voice channels'),
-    new SlashCommandBuilder().setName('addtelegram').setDescription('Add telegram bot'),
-    new SlashCommandBuilder().setName('idinahui').setDescription('Go nahui to random user from voice channel')
-].map(command => command.toJSON());
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on("guildCreate", guild => {
-    guild.channels.cache.some(channel => {
-        if (channel.type == 0) {
-            channel.send('Приветик! Я тут поселюсь, чтобы отправлять в телеграм информацию об активных пользователях в голосовых каналах')
-            return true
-        }
-    });
-
-    const rest = new REST({ version: '9' }).setToken(token);
-
-    rest.put(Routes.applicationGuildCommands(clientId, guild.id), { body: commands })
-        .then(() => console.log('Successfully registered application commands.'))
-        .catch(console.error);
-});
-
-
-client.on('interactionCreate', async interaction => {
-	if (!(interaction.type === InteractionType.ApplicationCommand)) return;
-
-	const { commandName } = interaction;
-
-	if (commandName === 'countusers') {
-        text = checkVoiceChannels(interaction.guild)
-        await interaction.reply(text);
-	} else if (commandName == 'addtelegram') {
-        await interaction.reply(interaction.guild.id);
-	} else if (commandName == 'idinahui') {
-        let members = []
-        interaction.guild.channels.cache.forEach(channel => {
-            if (channel.type == 2 && channel.members.size != 0) {
-                channel.members.forEach(member => {
-                    members.push(member.displayName)
-                })
-            } else return;
-        })
-        if (members[0] == undefined) {
-            await interaction.reply(`${interaction.member.displayName}, пошел нахуй!`)
-        } else {
-            await interaction.reply(`${members[Math.floor(Math.random()*members.length)]}, пошёл нахуй!`)
-        }
-        
-    }
-});
-
-function checkVoiceChannels(guild) {
-    let text = ""
-        guild.channels.cache.forEach(channel => {
-            if (channel.type == 2 && channel.members.size != 0) {
-                text += channel.name + ': ' + channel.members.size + '\n'
-                channel.members.forEach(member => {
-                    text += '   '
-                    text += member.displayName
-                    if (member.voice.selfDeaf) text += ' - выкл. звук'
-                    else if (member.voice.selfMute) text += ' - выкл. микрофон'
-                    text += '\n'
-                })
-            } else return;
-        })
-        
-        if (text == "") text = "В голосовых каналах нет участников"
-
-    return text
-}
-
-client.login(dsToken);
-
-
-////////////////
 
 const start = async () => {
 	try {
@@ -127,7 +41,6 @@ const start = async () => {
 
 			if (text === '/info') {
 				const user = await UserModel.findOne({chatId})
-                console.log(user.guildId)
                 let guild = client.guilds.cache.get(user.guildId)
                 let text = checkVoiceChannels(guild)
 				return bot.sendMessage(chatId, text)
